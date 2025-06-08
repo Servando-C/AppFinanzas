b14f4aea5cd5357182694618bc4ebb5b6d0f0beb
eddspicy <eduardo.cervantes@fi.unam.edu>
2025-06-08 05:26:28 +0000

    fix(prod):the page was deplyed but has a lot of issues with the frontend

--- finanzas-app/src/components/pages/Login.jsx
+++ finanzas-app/src/components/pages/Login.jsx
@@ -109,7 +109,7 @@ export default function Login(props) {
       password: password,
     };
 
-    fetch('http://127.0.0.1:5000/auth/login', { 
+    fetch('http://127.0.0.1:5000/api/auth/login', { 
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
--- finanzas-app/src/components/pages/SignUp.jsx
+++ finanzas-app/src/components/pages/SignUp.jsx
@@ -17,7 +17,7 @@ const SignUp = () => {
   const [errors, setErrors] = useState({});
 
   useEffect(() => {
-  fetch('http://127.0.0.1:5000/reportes/empresas')
+  fetch('http://127.0.0.1:5000/api/reportes/empresas')
       .then(response => response.json())
       .then(data => {
         setEmpresas(data.empresas);
@@ -49,7 +49,7 @@ const SignUp = () => {
     e.preventDefault();
     if (!validate()) return;
 
-    fetch('http://127.0.0.1:5000/auth/signup', {
+    fetch('http://127.0.0.1:5000/api/auth/signup', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
